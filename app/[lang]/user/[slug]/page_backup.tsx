f3d15fd26b1612cb11df43fa093eadfa67c33793
hazeflame <myhazeflame@gmail.com>
2025-09-13 17:12:55 +0800

    ref: favicon

--- app/[lang]/user/[slug]/page_backup.tsx
+++ app/[lang]/user/[slug]/page_backup.tsx
@@ -251,7 +251,6 @@ export default async function UserProfilePage({params}: {params: {lang: Locale, 
                       : 'text-gray-400 hover:text-white hover:bg-gray-700'
                   }`}
                 >
-                  <Icon className="w-5 h-5 mr-2" />
                   <span className="hidden sm:inline">{tab.label}</span>
                   <span className="sm:hidden">{tab.label.split(' ')[0]}</span>
                 </button>
